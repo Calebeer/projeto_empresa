e95953058231752e1662496528da326cb7ef3a29
calebeer <sostenesapollo55@gmail.com>
2023-08-01 12:26:34 -0300

    mexendo no projectcard

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,22 +1,31 @@
 import { useLocation } from "react-router-dom";
 import Message from "../layout/Message";
+import styles from './Project.module.css'
+import Container from '../layout/Container'
+import LinkButton from '../layout/LinkButton'
+import ProjectCard from "../project/ProjectCard";
 
 function Project(){
     
     const location = useLocation()
     let message = ''
     
-    console.log('>', location.state);
     if (location.state){
         message = location.state.message
     }
 
     return(
-        <div>
-        <h1>Meus Projetos</h1>
+        <div className={styles.project_container} >
+            <div className={styles.title_container} >
+                <h1>Meus Projetos</h1>
+                <LinkButton to={'/newproject'} text={'Novo projeto'} />
+            </div>
         {message &&  <Message type={'success'} msg={message}/>}
+        <Container customClass="start">
+            <ProjectCard/>
+        </Container>
         </div>
-
+            
     )
 }
 
---
+++ src/components/project/ProjectCard.js
@@ -0,0 +1,11 @@
+import styles from './ProjectCard.module.css'
+import {BsPencil, BsFillTrashFill} from 'react-icons'
+
+
+function ProjectCard({id, name, budget, category, handleRemove}){
+    return(
+        <div>Project</div>
+    )
+}
+
+export default ProjectCard;
